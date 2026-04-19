ba19688b87e88c5976347600a0ccb8832b29e952
yakujaae-afk <yakujaae@gmail.com>
2026-04-18 22:19:25 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,25 +1,5 @@
+import os
 
-
-from os import environ
-
-API_ID = int(environ.get("30296254", ""))
-API_HASH = environ.get("c2b5306f4ccd2d795405a026c10b4c62", "")
-BOT_TOKEN = environ.get("8631709476:AAEcmTI71C7aE3W_iBtzwF2WwRNjMWZQ93E", "")
-
-# Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "bot_subscription")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/bot_subscription")  # Channel link
-
-# Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "").split()))
-
-# Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", ""))
-
-# Database URL (if you want to add database support later)
-DATABASE_URL = environ.get("DATABASE_URL", "")
-
-
-
-
-
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
